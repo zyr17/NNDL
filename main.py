7ac334917419db59f11ad2ca17ba3e977323d1d6
zyr17 <jzjqz17@gmail.com>
2019-01-23 15:29:02 +0800

    alldata

--- main.py
+++ main.py
@@ -173,9 +173,72 @@ def SST2(modelfunc):
                       )
     trainer.train()
     
-def TWO(data, label, modelfunc):
+class CNNText(torch.nn.Module):
+    """
+    Text classification model by character CNN, the implementation of paper
+    'Yoon Kim. 2014. Convolution Neural Networks for Sentence
+    Classification.'
+    """
+
+    def __init__(self, embed_num,
+                 embed_dim,
+                 num_classes,
+                 kernel_nums=(3, 4, 5),
+                 kernel_sizes=(3, 4, 5),
+                 padding=0,
+                 dropout=0.5):
+        super(CNNText, self).__init__()
+
+        # no support for pre-trained embedding currently
+        self.embed = fastNLP.modules.encoder.Embedding(embed_num, embed_dim)
+        self.conv_pool = fastNLP.modules.encoder.ConvMaxpool(
+            in_channels=embed_dim,
+            out_channels=kernel_nums,
+            kernel_sizes=kernel_sizes,
+            padding=padding)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.fc = fastNLP.modules.encoder.Linear(sum(kernel_nums), num_classes)
+
+    def forward(self, word_seq):
+        """
+
+        :param word_seq: torch.LongTensor, [batch_size, seq_len]
+        :return output: dict of torch.LongTensor, [batch_size, num_classes]
+        """
+        if word_seq.size(1) < 5:
+            #print('word_seq ', word_seq)
+            zeros = torch.zeros(word_seq.size(0), 5).long().cuda()
+            #print('zeros ', zeros.size())
+            zeros[:, :word_seq.size(1)] = word_seq
+            word_seq = zeros
+            #print('word_seq ', word_seq)
+        #print(word_seq, word_seq.size())
+        x = self.embed(word_seq)  # [N,L] -> [N,L,C]
+        #print(x.size())
+        x = self.conv_pool(x)  # [N,L,C] -> [N,C]
+        #print(x.size())
+        x = self.dropout(x)
+        #print(x.size())
+        x = self.fc(x)  # [N,C] -> [N, N_class]
+        #print(x.size())
+        return {'pred': x}
+
+    def predict(self, word_seq):
+        """
+
+        :param word_seq: torch.LongTensor, [batch_size, seq_len]
+        :return predict: dict of torch.LongTensor, [batch_size, seq_len]
+        """
+        output = self(word_seq)
+        _, predict = output['pred'].max(dim=1)
+        return {'pred': predict}
+    
+def RUN(data, label, split, modelfunc, classnum = 2, epochs = 10):
     assert(len(data) == len(label))
-    dataset = fastNLP.DataSet({'raw_sentence': data, 'label_str': label})
+    if split == None:
+        dataset = fastNLP.DataSet({'raw_sentence': data, 'label_str': label})
+    else:
+        dataset = fastNLP.DataSet({'raw_sentence': data, 'label_str': label, 'split': split})
     dataset.drop(lambda x: len(x['raw_sentence']) == 0)
     #[dataset.append(fastNLP.DataSet({'raw_sentence': data[x], 'label': label[x]})) for x in range(len(data))]
     dataset.apply(lambda x: int(float(x['label_str'])), new_field_name='label', is_target=True)
@@ -184,13 +247,21 @@ def TWO(data, label, modelfunc):
     vocab = fastNLP.Vocabulary(min_freq = 1)
     dataset.apply(lambda x: [vocab.add(word) for word in x['word_str']])
     
-    traindata, testdata = dataset.split(0.1)
-    print(len(traindata), len(testdata))
+    if split == None:
+        traindata, testdata = dataset.split(0.1)
+        #print(len(traindata), len(testdata))
+    else:
+        traindata = dataset[:]
+        testdata = dataset[:]
+        traindata.drop(lambda x: x['split'] != 'train')
+        testdata.drop(lambda x: x['split'] != 'test')
+        
+    #print(len(traindata), len(testdata))
     
     traindata.apply(lambda x: [vocab.to_index(word) for word in x['word_str']], new_field_name='word_seq', is_input=True)
     testdata.apply(lambda x: [vocab.to_index(word) for word in x['word_str']], new_field_name='word_seq', is_input=True)
     
-    model = modelfunc(embed_num=len(vocab),embed_dim=100,num_classes=2,kernel_nums=(3,4,5), kernel_sizes=(3,4,5),padding=0,dropout=0)
+    model = modelfunc(embed_num=len(vocab),embed_dim=100,num_classes=classnum,kernel_nums=(3,4,5), kernel_sizes=(3,4,5),padding=0,dropout=0)
     model.embed.dropout=torch.nn.Dropout(0.5)
 
     trainer = fastNLP.Trainer(model=model, 
@@ -199,15 +270,24 @@ def TWO(data, label, modelfunc):
                       loss=fastNLP.CrossEntropyLoss(),
                       metrics=fastNLP.AccuracyMetric(),
                       use_cuda = True,
-                      n_epochs=10
+                      n_epochs=epochs,
+                      check_code_level=-1
                       )
     trainer.train()
 
 
 pickle = pandas.read_pickle('data/MR.pkl')
 MRdata, MRlabel = list(pickle.sentence), list(pickle.label)
+pickle = pandas.read_pickle('data/SST1.pkl')
+SST1data, SST1label, SST1split = list(pickle.sentence), list(pickle.label), list(pickle.split)
+pickle = pandas.read_pickle('data/SST2.pkl')
+SST2data, SST2label, SST2split = list(pickle.sentence), list(pickle.label), list(pickle.split)
+pickle = pandas.read_pickle('data/TREC.pkl')
+TRECdata, TREClabel, TRECsplit = list(pickle.sentence), list(pickle.label), list(pickle.split)
 pickle = pandas.read_pickle('data/SUBJ.pkl')
 SUBJdata, SUBJlabel = list(pickle.sentence), list(pickle.label)
+pickle = pandas.read_pickle('data/TREC.pkl')
+TRECdata, TREClabel, TRECsplit = list(pickle.sentence), list(pickle.label), list(pickle.split)
 pickle = pandas.read_pickle('data/CR.pkl')
 CRdata, CRlabel = list(pickle.sentence), list(pickle.label)
 pickle = pandas.read_pickle('data/MPQA.pkl')
@@ -216,10 +296,10 @@ MPQAdata, MPQAlabel = list(pickle.sentence), list(pickle.label)
 #print(len(MPQAdata), len(MPQAlabel))
 #print(MPQAdata[10110], MPQAlabel[10110])
 
-#TWO(MRdata, MRlabel, fastNLP.models.CNNText)
-#SST1(fastNLP.models.CNNText)
-#SST2(fastNLP.models.CNNText)
-#TWO(SUBJdata, SUBJlabel, fastNLP.models.CNNText)
-
-#TWO(CRdata, CRlabel, fastNLP.models.CNNText)
-TWO(MPQAdata, MPQAlabel, fastNLP.models.CNNText)
+#RUN(MRdata, MRlabel, None, CNNText)
+#RUN(SST1data, SST1label, SST1split, CNNText, 5)
+#RUN(SST2data, SST2label, SST2split, CNNText)
+#RUN(SUBJdata, SUBJlabel, None, CNNText)
+#RUN(TRECdata, TREClabel, TRECsplit, CNNText, 6, 100)
+#RUN(CRdata, CRlabel, CNNText)
+#RUN(MPQAdata, MPQAlabel, None, CNNText)
